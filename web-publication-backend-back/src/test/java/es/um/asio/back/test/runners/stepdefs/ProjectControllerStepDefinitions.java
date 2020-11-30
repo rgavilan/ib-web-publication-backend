294f2d31f2e5a5625de52e7d2e029a69e6bd9004
Atorrecilla <atorrecilla@izertis.com>
2020-11-30 09:17:48 +0100

    [SEMANTMURC-383] Test Controller Project fix Fuseki

--- web-publication-backend-back/src/test/java/es/um/asio/back/test/runners/stepdefs/ProjectControllerStepDefinitions.java
+++ web-publication-backend-back/src/test/java/es/um/asio/back/test/runners/stepdefs/ProjectControllerStepDefinitions.java
@@ -1,6 +1,7 @@
 package es.um.asio.back.test.runners.stepdefs;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,7 @@ import org.springframework.data.domain.Sort;
 
 import es.um.asio.back.controller.project.ProjectController;
 import es.um.asio.service.filter.project.ProjectFilter;
+import es.um.asio.service.model.FusekiResponse;
 import es.um.asio.service.proxy.project.ProjectProxy;
 import es.um.asio.service.proxy.project.impl.ProjectProxyImpl;
 import es.um.asio.service.service.project.ProjectService;
@@ -51,14 +53,21 @@ public class ProjectControllerStepDefinitions {
 	}
 
 	@Before
-	public void setup() throws Exception {
+	public void setUp() throws Exception {
+		filter = new ProjectFilter();
+		filter.setName("NAME");
+		filter.setLanguage("es");
 
+		pageable = PageRequest.of(1, 5, Sort.by("ASC"));
 		// Mock StorageType proxy
 		Mockito.when(this.proxy.findPaginated(filter, pageable)).thenAnswer(invocation -> {
 
-			String json = "{\r\n" + "  \"head\": {\r\n"
+			FusekiResponse fuseki = new FusekiResponse();
+			List<FusekiResponse> contentResult = new ArrayList<>();
+			String head = "{\r\n" + "  \"head\": {\r\n"
 					+ "    \"vars\": [ \"x\" , \"name\" , \"description\" , \"ini\" , \"fin\" , \"id\" , \"tipo\" ]\r\n"
-					+ "  } ,\r\n" + "  \"results\": {\r\n" + "    \"bindings\": [\r\n" + "      {\r\n"
+					+ "  } ,\r\n";
+			String result = "  \"results\": {\r\n" + "    \"bindings\": [\r\n" + "      {\r\n"
 					+ "        \"x\": { \"type\": \"uri\" , \"value\": \"http://hercules.org/um/es-ES/rec/Proyecto/c1cca9b3-6762-3e09-8aa3-ea860644ecd4\" } ,\r\n"
 					+ "        \"name\": { \"type\": \"literal\" , \"xml:lang\": \"es\" , \"value\": \"NAME\" } ,\r\n"
 					+ "        \"description\": { \"type\": \"literal\" , \"xml:lang\": \"es\" , \"value\": \"FP00-359\" } ,\r\n"
@@ -67,10 +76,11 @@ public class ProjectControllerStepDefinitions {
 					+ "        \"id\": { \"type\": \"literal\" , \"xml:lang\": \"es\" , \"value\": \"15076\" } ,\r\n"
 					+ "        \"tipo\": { \"type\": \"literal\" , \"xml:lang\": \"es\" , \"value\": \"FP00\" }\r\n"
 					+ "      }\r\n" + "    ]\r\n" + "  }\r\n" + "}";
-
-			List<String> list = new ArrayList<>();
-			list.add(json);
-			Page<String> page = new PageImpl<>(list, pageable, list.size());
+			;
+			fuseki.setHead(head);
+			fuseki.setResults(result);
+			contentResult.add(fuseki);
+			Page<FusekiResponse> page = new PageImpl<>(contentResult, pageable, contentResult.size());
 			return page;
 		});
 
@@ -78,18 +88,16 @@ public class ProjectControllerStepDefinitions {
 
 	@Given("^call to backend project controller$")
 	public void call_to_backend_project_controller() {
-		filter = new ProjectFilter();
-		filter.setName("NAME");
-		filter.setLanguage("es");
-
-		pageable = PageRequest.of(1, 5, Sort.by("ASC"));
+		assertNotNull(filter);
 	}
 
 	@Then("^the controller call to servie and find result to send front$")
 	public void the_controller_call_to_servie_and_find_result_to_send_front() {
 
-		Page<String> page = this.proxy.findPaginated(filter, pageable);
-		assertNull(page);
+		Page<FusekiResponse> page = this.proxy.findPaginated(filter, pageable);
+		assertNotNull(page);
+
+		assertEquals(true, page.getContent().get(0).getResults().toString().contains("NAME"));
 	}
 
 }
